4004d931a057552c3eee5fe5b0c6a6cdfe122d03
Vanessa <v.barajas1995@gmail.com>
2023-05-15 15:34:47 -0700

    Final Project submission

---
+++ FinalSpring23/src/AnimalClass.java
@@ -0,0 +1,143 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class AnimalClass {
+    private String uniqueID;
+    private String species;
+    private String name;
+    private int age;
+    private String birthDate;
+    private String gender;
+    private String birthSeason;
+    private int weight;
+    private String origin;
+    private String color;
+    private String animalName;
+    private String getDay;
+
+    public AnimalClass (String species, int age, String gender, String birthSeason, int weight, String origin, String color) {
+        this.species = species;
+        this.age = age;
+        this.gender = gender;
+        this.birthSeason = birthSeason;
+        this.weight = weight;
+        this.origin = origin;
+        this.color = color;
+    }
+    @Override
+    public String toString() {
+        return uniqueID + "; " + animalName + "; " + age + " years old; birth date " + birthDate + "; " + color + "; " + gender + "; " + weight + " pounds; " + origin + "; arrived " + getDay;
+    }
+    public String getUniqueID() {
+        return uniqueID;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getBirthDate() {
+        return birthDate;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getBirthSeason() {
+        return birthSeason;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public String getOrigin() {
+        return origin;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getAnimalName() {
+        return animalName;
+    }
+    public String getGetDay() {
+        return getDay;
+    }
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setBirthSeason(String birthSeason) {
+        this.birthSeason = birthSeason;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+    public void setGetDay(String getDay) {
+        this.getDay = getDay;
+    }
+    public String generateBirthDay(int age, String birthSeason) {
+        Random random = new Random();
+        int birthYear = 2023 - age;
+        int birthMonth = 0;
+        int birthDay = 0;
+
+        if (birthSeason.equals("Spring")){
+            birthMonth = 3 + random.nextInt(3);
+            birthDay = 1 + random.nextInt(30);
+        } else if (birthSeason.equals("Summer")) {
+            birthMonth = 5 + random.nextInt(3);
+            birthDay = 1 + random.nextInt(30);
+        } else if (birthSeason.equals("Spring")) {
+            birthMonth = 8 + random.nextInt(3);
+            birthDay = 1 + random.nextInt(30);
+        } else if (birthSeason.equals("Spring")) {
+            birthMonth = 11 + random.nextInt(2);
+            birthDay = 1 + random.nextInt(30);
+        } else {
+            birthMonth = random.nextInt(12) + 1;
+            birthDay = random.nextInt(30) + 1;
+        }
+        if(birthMonth == 2 && (birthDay == 29 || birthDay == 30)){
+            birthDay = 28;
+        }
+        LocalDate birthdate = LocalDate.of(birthYear, birthMonth, birthDay);
+        return birthdate.format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+    }
+    public String generateGetDay() {
+        LocalDate getDate = java.time.LocalDate.now();
+        return getDate.format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+    }
+    public String generateID(String species, int Counter) {
+        return species.substring(0,2) + "0" + Counter;
+    }
+    public String generateName(String[] names) {
+        Random random = new Random();
+        return names[random.nextInt(names.length)];
+    }
+}
---
+++ FinalSpring23/src/Spring23Final.java
@@ -0,0 +1,109 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Spring23Final {
+    private static Object IOException;
+
+    public static void main(String[] args) throws RuntimeException, IOException {
+
+
+        //Create linked lists for the habitats
+        LinkedList<AnimalClass> hyenaHabitat = new LinkedList<>();
+        LinkedList<AnimalClass> lionHabitat = new LinkedList<>();
+        LinkedList<AnimalClass> tigerHabitat = new LinkedList<>();
+        LinkedList<AnimalClass> bearHabitat = new LinkedList<>();
+
+        String[] hyenaNames = {"Shenzi", "Banzai", "Ed", "Zig", "Bud", "Lou", "Kamari", "Wema", "Nne", "Madoa", "Prince Nevarah"};
+        String[] lionNames = {"Scar", "Mufasa", "Simba", "Kiara", "King", "Drooper", "Kimba", "Nala", "Leo", "Samson", "Elsa", "Cecil"};
+        String[] tigerNames = {"Tony", "Tigger", "Amber", "Cosimia", "Cuddles", "Dave", "Jiba", "Rajah", "Rayas", "Ryker"};
+        String[] bearNames = {"Yogi", "Smokey", "Paddington", "Lippy", "Bungle", "Baloo", "Rupert", "Winnie the Pooh", "Snuggles", "Bert"};
+
+        int hyenaCounter = 1;
+        int lionCounter = 1;
+        int tigerCounter = 1;
+        int bearCounter = 1;
+
+        File inputFile = new File("/Users/Vanessa/MyGitDir/final-program-vbarajas95/arrivingAnimals.txt");
+        FileWriter writer;
+        try (Scanner scanner = new Scanner(inputFile)) {
+            try {
+                writer = new FileWriter("zooPopulation.txt");
+            } catch (java.io.IOException e) {
+                throw new RuntimeException(e);
+            }
+            writer.write("Final Program Output by Vanessa Barajas, Spring 2023\n\n");
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(", ");
+                String[] ageData = data[0].split(" ");
+
+                int age = Integer.parseInt(ageData[0]);
+                String sex = ageData[3];
+                String species = ageData[4];
+                String birthSeason = "unknown";
+                String animalName = "";
+                if (ageData.length == 6) {
+                    birthSeason = ageData[5];
+                }
+                String color = data[2];
+                int weight = Integer.parseInt(data[3].split("")[0]);
+                String origin = data[4] + ", " + data[5];
+
+                AnimalClass temp = new AnimalClass(species, age, sex, birthSeason, weight, origin, color);
+
+                switch (species) {
+                    case "hyena":
+                        temp.setName(temp.generateName(hyenaNames));
+                        hyenaHabitat.add(temp);
+                        temp.setUniqueID(temp.generateID(temp.getSpecies(), hyenaCounter));
+                        hyenaCounter++;
+                        break;
+                    case "lion":
+                        temp.setName(temp.generateName(lionNames));
+                        lionHabitat.add(temp);
+                        temp.setUniqueID(temp.generateID(temp.getSpecies(), lionCounter));
+                        lionCounter++;
+                        break;
+                    case "tiger":
+                        temp.setName(temp.generateName(tigerNames));
+                        tigerHabitat.add(temp);
+                        temp.setUniqueID(temp.generateID(temp.getSpecies(), tigerCounter));
+                        tigerCounter++;
+                        break;
+                    case "bear":
+                        temp.setName(temp.generateName(bearNames));
+                        bearHabitat.add(temp);
+                        temp.setUniqueID(temp.generateID(temp.getSpecies(), bearCounter));
+                        bearCounter++;
+                        break;
+                }
+                temp.setBirthDate(temp.generateBirthDay(age, birthSeason));
+                temp.setGetDay(temp.generateGetDay());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        writer.write("\n\nHyena Habitat:\n\n");
+        for (AnimalClass animal : hyenaHabitat) {
+            writer.write(animal.toString() + "\n");
+        }
+        writer.write("\n\nLion Habitat:\n\n");
+        for (AnimalClass animal : lionHabitat) {
+            writer.write(animal.toString() + "\n");
+        }
+        writer.write("\n\nTiger Habitat:\n\n");
+        for (AnimalClass animal : tigerHabitat) {
+            writer.write(animal.toString() + "\n");
+        }
+        writer.write("\n\nBear Habitat:\n\n");
+        for (AnimalClass animal : bearHabitat) {
+            writer.write(animal.toString() + "\n");
+        }
+        writer.close();
+    }
+}
